f8135561e3b03f432ccd16eacd718498f33966c6
FaysalNemesis <faysalahammednstu13@gmail.com>
2025-01-26 15:34:53 +0600

    created store project and product model

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreCURDApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(150)]
+        public string Description { get; set; }
+        [MaxLength(50)]
+        public string Brand { get; set; }
+        [MaxLength(100)]
+        public string Category { get; set; }
+        [Precision(16 ,2)]
+        public decimal Price { get; set; }
+        [MaxLength(100)]
+        public string ProductImageName { get; set; }
+        public DateTime creaetedAt { get; set; }
+
+
+    }
+}
